b96cfcada72566d146d5fc31e04326afb05037f4
chsadmin <chsadmin@192.168.86.1>
2018-07-14 08:13:30 +0800

    start

--- testNode1/testJS.js
+++ testNode1/testJS.js
@@ -6,13 +6,40 @@ size(400, 400);
 frameRate(30);
 
 
-//ProgramCodeGoesHere
-//bottom circle:
-ellipse(200, 300, 150, 150);
-// middle circle:
-ellipse(200, 225, 100, 100);
-// top circle:
-ellipse(200, 175, 63, 63);
+    //ProgramCodeGoesHere
+
+    // Adapted from Dan Shiffman, natureofcode.com
+
+    var Walker = function() {
+        this.x = width/2;
+        this.y = height/2;
+    };
+
+    Walker.prototype.display = function() {
+        stroke(0, 0, 0);
+        point(this.x, this.y);
+    };
+
+    // Randomly move up, down, left, right, or stay in one place
+    Walker.prototype.walk = function() {
+        var choice = floor(random(4));
+        if (choice === 0) {
+            this.x++;
+        } else if (choice === 1) {
+            this.x--;
+        } else if (choice === 2) {
+            this.y++;
+        } else {
+            this.y--;
+        }
+    };
+
+    var w = new Walker();
+
+    var draw = function() {
+        w.walk();
+        w.display();
+    };
 
 
 
