64352c499af9b36ea7dfcee519f4140cb3153e70
chsadmin <chsadmin@192.168.86.1>
2018-07-14 08:44:37 +0800

    clean code

--- testNode1/testJS.js
+++ testNode1/testJS.js
@@ -14,32 +14,26 @@ frameRate(30);
         this.x = width/2;
         this.y = height/2;
 
-        this.workerWidth = 7;
+        this.width = 7;
     };
 
     Walker.prototype.display = function() {
-        stroke(0, 0, 0);
-        stroke(random(256), random(256), random(256));
         stroke(100, random(100), random(100));
-//        noStroke();
-        fill(random(256), random(256), random(256));
-        fill(random(100), 0, random(100) + 150);
         fill(random(100) + 150, 100, random(100) + 150);
-        rect(this.x+1, this.y+1, this.workerWidth-2, this.workerWidth-2);
-//        point(this.x, this.y);
+        rect(this.x+1, this.y+1, this.width-2, this.width-2);
     };
 
     // Randomly move up, down, left, right, or stay in one place
     Walker.prototype.walk = function() {
         var choice = floor(random(4));
         if (choice === 0) {
-            this.x+=this.workerWidth;
+            this.x+=this.width;
         } else if (choice === 1) {
-            this.x-=this.workerWidth;
+            this.x-=this.width;
         } else if (choice === 2) {
-            this.y+=this.workerWidth;
+            this.y+=this.width;
         } else {
-            this.y-=this.workerWidth;
+            this.y-=this.width;
         }
     };
 
