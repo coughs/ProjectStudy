f47457372f90ca79ae82f2d1e6e89d35b3fd4d55
chsadmin <chsadmin@192.168.86.1>
2018-07-14 08:27:24 +0800

    update 2

--- testNode1/testJS.js
+++ testNode1/testJS.js
@@ -17,20 +17,28 @@ frameRate(30);
 
     Walker.prototype.display = function() {
         stroke(0, 0, 0);
-        point(this.x, this.y);
+        stroke(random(256), random(256), random(256));
+        stroke(100, random(100), random(100));
+//        noStroke();
+        fill(random(256), random(256), random(256));
+        fill(random(100), 0, random(100) + 150);
+        fill(random(100) + 150, 100, random(100) + 150);
+        rect(this.x+1, this.y+1, workerWidth-2, workerWidth-2);
+//        point(this.x, this.y);
     };
 
+    var workerWidth = 7;
     // Randomly move up, down, left, right, or stay in one place
     Walker.prototype.walk = function() {
         var choice = floor(random(4));
         if (choice === 0) {
-            this.x++;
+            this.x+=workerWidth;
         } else if (choice === 1) {
-            this.x--;
+            this.x-=workerWidth;
         } else if (choice === 2) {
-            this.y++;
+            this.y+=workerWidth;
         } else {
-            this.y--;
+            this.y-=workerWidth;
         }
     };
 
