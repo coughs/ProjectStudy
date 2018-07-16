538336a0e019d3ed5aa9c898ea8a6c359aff0c37
conghs <conghs@ieslab.cn>
2018-07-16 10:55:09 +0800

    限制范围的版本

--- testNode1/testJS.js
+++ testNode1/testJS.js
@@ -2,7 +2,7 @@
  *
  */
 var sketchProc=function(processingInstance){ with (processingInstance){
-size(400, 400);
+size(800, 700);
 frameRate(30);
 
 
@@ -18,35 +18,48 @@ frameRate(30);
     };
 
     Walker.prototype.display = function() {
-        stroke(100, random(100), random(100));
+        stroke(0, 1+random(10), 1+random(10));
         fill(random(100) + 150, 100, random(100) + 150);
-        rect(this.x+1, this.y+1, this.width-2, this.width-2);
+        rect(this.x, this.y, this.width-2, this.width-2);
     };
 
     // Randomly move up, down, left, right, or stay in one place
     Walker.prototype.walk = function() {
         var choice = floor(random(4));
         if (choice === 0) {
-            this.x+=this.width;
+            if (this.x<width-this.width*3) {
+                this.x+=this.width;
+            }
         } else if (choice === 1) {
-            this.x-=this.width;
+            if (this.x>this.width*2) {
+                this.x -= this.width;
+            }
         } else if (choice === 2) {
-            this.y+=this.width;
+            if (this.y<height-this.width*3) {
+                this.y+=this.width;
+            }
         } else {
-            this.y-=this.width;
+            if (this.width*2 < this.y) {
+                this.y -= this.width;
+            }
         }
     };
 
     var w = new Walker();
 
-    var draw = function() {
+    draw = function() {
+
         w.walk();
+
         w.display();
-    };
+
+/*
+        background(100,100,100);
+        text("X: "+mouseX+",Y:"+mouseY,20,20);
+*/ };
 
 
 
 }};
-
 
 
